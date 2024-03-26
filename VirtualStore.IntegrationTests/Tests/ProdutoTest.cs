3d10e39254c87f6043b41c416ce1536a93f137ef
eazevedo016 <eazevedo.016@gmail.com>
2024-03-26 00:13:05 -0400

    finalizando testes de integracao para todas as chamadas da controller

--- VirtualStore.IntegrationTests/Tests/ProdutoTest.cs
+++ VirtualStore.IntegrationTests/Tests/ProdutoTest.cs
@@ -16,7 +16,32 @@ namespace VirtualStore.IntegrationTests.Tests
     public class ProdutoTest : TestBase
     {
         public ProdutoTest(WebAppFactory factory) : base(factory) { }
-        
+
+        [Fact]
+        public async Task CreateProduto()
+        {
+
+            //Arrange
+            var produto = new Produto()
+            {
+                Nome = "Produto Teste",
+                Estoque = 10,
+                Valor = 5800
+            };
+
+            var stringPayload = JsonConvert.SerializeObject(produto);
+            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("/produtos", httpContent);
+
+            // Act
+            var res = await response.Content.ReadFromJsonAsync<Produto>();
+
+            // Assert
+            Assert.Equal(res.Nome, produto.Nome);
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+        }
+
         [Fact]
         public async Task GetProduto()
         {
@@ -27,58 +52,176 @@ namespace VirtualStore.IntegrationTests.Tests
             response.EnsureSuccessStatusCode(); // Status Code 200-299
         }
 
+
+        [Fact]
+        public async Task UpdateProduto()
+        {
+
+            //Arrange
+            var produto = new Produto()
+            {
+                Nome = "Criação de novo produto para atender ao teste de integração UpdateProduto",
+                Estoque = 1,
+                Valor = 1
+            };
+
+            
+            var stringPayloadCreate = JsonConvert.SerializeObject(produto);
+            var httpContentCreate = new StringContent(stringPayloadCreate, Encoding.UTF8, "application/json");
+            var createResponse = await _client.PostAsync("/produtos", httpContentCreate);
+            createResponse.EnsureSuccessStatusCode();
+            var produtoCriado = await createResponse.Content.ReadFromJsonAsync<Produto>();
+
+            produtoCriado.Nome = "Produto Atualizado";
+            produtoCriado.Estoque = 30;
+            produtoCriado.Valor = 7500;
+
+            var stringPayloadUpdate = JsonConvert.SerializeObject(produtoCriado);
+            var httpContentUpdate = new StringContent(stringPayloadUpdate, Encoding.UTF8, "application/json");
+
+            try
+            {   // Act
+                var updateResponse = await _client.PutAsync($"/produtos/{produtoCriado.Id}", httpContentUpdate);
+                updateResponse.EnsureSuccessStatusCode();
+                var updatedProduto = await updateResponse.Content.ReadFromJsonAsync<Produto>();
+
+                // Assert
+                Assert.Equal(produtoCriado.Id, updatedProduto.Id);
+                Assert.Equal(produtoCriado.Nome, updatedProduto.Nome);
+                Assert.Equal(produtoCriado.Estoque, updatedProduto.Estoque);
+                Assert.Equal(produtoCriado.Valor, updatedProduto.Valor);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Erro na solicitação HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+
+        [Fact]
+        public async Task DeleteProduto()
+        {
+
+            //Arrange
+            var produto = new Produto()
+            {
+                Nome = "Criação de novo produto para atender ao teste de integração DeleteProduto",
+                Estoque = 2,
+                Valor = 2
+            };
+
+
+            var stringPayloadCreate = JsonConvert.SerializeObject(produto);
+            var httpContentCreate = new StringContent(stringPayloadCreate, Encoding.UTF8, "application/json");
+            var createResponse = await _client.PostAsync("/produtos", httpContentCreate);
+            createResponse.EnsureSuccessStatusCode();
+            var produtoCriado = await createResponse.Content.ReadFromJsonAsync<Produto>();
+
+            try
+            {
+                //Act
+                var deleteResponse = await _client.DeleteAsync($"/produtos/{produtoCriado.Id}");
+
+                // Assert
+                deleteResponse.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Erro na solicitação HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+
+
         [Fact]
         public async Task GetProdutoById()
         {
-            // Act
-            var response = await _client.GetAsync("/produtos/4");
-
-            var produto = new Produto()
+            // Arrange
+            var novoProduto = new Produto()
             {
-                Nome = "SSD",
-                Estoque = 20,
-                Valor = 800
+                Nome = "Criação de novo produto para atender ao teste de integração GetProdutoById",
+                Estoque = 3,
+                Valor = 3
             };
 
-           
-            var res = await response.Content.ReadFromJsonAsync<Produto>();
+            var jsonPayload = JsonConvert.SerializeObject(novoProduto);
+            var httpContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
 
-            // Assert
-            Assert.Equal(res.Nome, produto.Nome);
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var postResponse = await _client.PostAsync("/produtos", httpContent);
+            postResponse.EnsureSuccessStatusCode();
+            
+            var produtoAdicionado = await postResponse.Content.ReadFromJsonAsync<Produto>();
+
+            try
+            {   
+                //Act
+                var getResponse = await _client.GetAsync($"/produtos/{produtoAdicionado.Id}");
+                getResponse.EnsureSuccessStatusCode();
+               
+                var produtoRetornado = await getResponse.Content.ReadFromJsonAsync<Produto>();
+
+                // Assert
+                Assert.Equal(produtoAdicionado.Nome, produtoRetornado.Nome);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Erro na solicitação HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
 
         }
 
 
         [Fact]
-        public async Task CreateProduto()
+        public async Task GetProdutoByName()
         {
-            var produto = new Produto()
+            // Arrange
+            var novoProduto = new Produto()
             {
-                Nome = "Produto Teste",
-                Estoque = 10,
-                Valor = 5800
+                Nome = "Criação de novo produto para atender ao teste de integração GetProdutoByName",
+                Estoque = 3,
+                Valor = 3
             };
 
-           
-            var stringPayload = JsonConvert.SerializeObject(produto);
+            var jsonPayload = JsonConvert.SerializeObject(novoProduto);
+            var httpContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
 
-            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+            var postResponse = await _client.PostAsync("/produtos", httpContent);
+            postResponse.EnsureSuccessStatusCode();
 
-            var response = await _client.PostAsync("/produtos", httpContent);
+            var produtoAdicionado = await postResponse.Content.ReadFromJsonAsync<Produto>();
 
-        
-            
-            var res = await response.Content.ReadFromJsonAsync<Produto>();
+            try
+            {
+                //Act
+                var getResponse = await _client.GetAsync($"/produtos/{produtoAdicionado.Nome}");
+                getResponse.EnsureSuccessStatusCode();
 
-            Assert.Equal(res.Nome, produto.Nome);
+                var produtoRetornado = await getResponse.Content.ReadFromJsonAsync<Produto>();
 
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
+                // Assert
+                Assert.Equal(produtoAdicionado.Nome, produtoRetornado.Nome);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Erro na solicitação HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
 
         }
-
-
-
-
     }
+
 }
+
--- VirtualStore.IntegrationTests/obj/Debug/net7.0/VirtualStore.IntegrationTests.AssemblyInfo.cs
+++ VirtualStore.IntegrationTests/obj/Debug/net7.0/VirtualStore.IntegrationTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtualStore.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac556b93f6411a42c71ca914fef5e8e3603f0b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3f5ad4f135f28075668bf76fa1b92ca6755c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtualStore.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtualStore.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
